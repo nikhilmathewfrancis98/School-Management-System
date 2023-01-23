c52287fdcf3fd95cef660ea8532af6e2137e63fe
Nikhil Mathew Francis <nikhilmathewfrancis@gmail.com>
2023-01-23 19:20:37 +0530

    Edited the Schema.sqland data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,13 @@
 --INSERT INTO holidays (holiday,reason,type)VALUES(' Sep 5 ','Labor Day','FEDERAL');
 --INSERT INTO holidays (holiday,reason,type)VALUES(' Nov 11 ','Veterans Day','FEDERAL');
 --
+
+INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
+  VALUES ('ADMIN',CURDATE(),'DBA');
+
+INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
+  VALUES ('STUDENT',CURDATE(),'DBA');
+
+
+INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
+  VALUES ('Admin','admin@eazyschool.com','3443434343','$2a$10$XhU4UcSxDPb5G0I0fT/CZ.Lfj2VW2fkLkUP5cOEM.xM8EzyUQXaD2', 1 ,CURDATE(),'DBA');
