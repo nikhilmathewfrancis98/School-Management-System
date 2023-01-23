18c1c12924199536b9cefadf7de51eef43695327
Nikhil Mathew Francis <nikhilmathewfrancis@gmail.com>
2023-01-23 19:26:17 +0530

    Edited the Schema.sqland data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
---INSERT INTO holidays (holiday,reason,type)VALUES('Jan 1','New-Year','FESTIVAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Oct 31 ','Halloween','FESTIVAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Nov 24 ','Thanksgiving Day','FESTIVAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Dec 25 ','Christmas Eve','FESTIVAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Jan 17 ','Sr Martin Luther King Day','FEDERAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' July 4 ','Independence Day','FEDERAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Sep 5 ','Labor Day','FEDERAL');
---INSERT INTO holidays (holiday,reason,type)VALUES(' Nov 11 ','Veterans Day','FEDERAL');
---
+INSERT INTO holidays (holiday,reason,type)VALUES('Jan 1','New-Year','FESTIVAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Oct 31 ','Halloween','FESTIVAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Nov 24 ','Thanksgiving Day','FESTIVAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Dec 25 ','Christmas Eve','FESTIVAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Jan 17 ','Sr Martin Luther King Day','FEDERAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' July 4 ','Independence Day','FEDERAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Sep 5 ','Labor Day','FEDERAL');
+INSERT INTO holidays (holiday,reason,type)VALUES(' Nov 11 ','Veterans Day','FEDERAL');
+
 
 INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
   VALUES ('ADMIN',CURDATE(),'DBA');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,27 @@
---CREATE TABLE IF NOT EXISTS `contact_msg` (
---  `contact_id` int AUTO_INCREMENT,
---  `name` varchar(100) NOT NULL,
---  `mobile_num` varchar(10) NOT NULL,
---  `email` varchar(100) NOT NULL,
---  `subject` varchar(100) NOT NULL,
---  `message` varchar(500) NOT NULL,
---  `status` varchar(10) NOT NULL,
---  `created_at` TIMESTAMP NOT NULL,
---  `created_by` varchar(50) NOT NULL,
---  `updated_at` TIMESTAMP DEFAULT NULL,
---  `updated_by` varchar(50) DEFAULT NULL,
---  PRIMARY KEY(`contact_id`)
---);
---
---CREATE TABLE IF NOT EXISTS `holidays`(
---`holiday` varchar(100) NOT NULL,
---`reason` varchar(100) NOT NULL,
---`type` ENUM('FEDERAL', 'FESTIVAL') NOT NULL
-----`type` varchar(100) NOT NULL
---);
+CREATE TABLE IF NOT EXISTS `contact_msg` (
+  `contact_id` int AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `mobile_num` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  PRIMARY KEY(`contact_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `holidays` (
+  `holiday` varchar(20) NOT NULL,
+  `reason` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
 
 CREATE TABLE IF NOT EXISTS `person` (
   `person_id` int NOT NULL AUTO_INCREMENT,
